33200bacd11e841dba7c0667f822e08a71f76e86
dimidia379 <j.zvereva050@gmail.com>
2021-12-28 20:59:25 +0300

    tests added

---
+++ src/test/java/guru/qa/DemoWSTests.java
@@ -0,0 +1,59 @@
+package guru.qa;
+
+import com.codeborne.selenide.Configuration;
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Cookie;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
+import static io.qameta.allure.Allure.step;
+import static io.restassured.RestAssured.given;
+
+public class DemoWSTests {
+
+    @BeforeAll
+    static void openPageWithAuthorization() {
+        RestAssured.baseURI = "http://demowebshop.tricentis.com";
+        Configuration.baseUrl = "http://demowebshop.tricentis.com";
+    }
+
+    @Test
+    void loginWithCookieToCheckUsersNamesTest () {
+
+        step("Get cookie by api and set it to browser", () -> {
+            String authorizationCookie =
+                    given()
+                            .contentType("application/x-www-form-urlencoded; charset=UTF-8")
+                            .formParam("Email", "harry@te.st")
+                            .formParam("Password", "qwerty")
+                            .when()
+                            .post("/login")
+                            .then()
+                            .statusCode(302)
+                            .extract()
+                            .cookie("NOPCOMMERCE.AUTH");
+
+        step("Open minimal content, because cookie can be set when site is opened", () ->
+                open("/Themes/DefaultClean/Content/images/logo.png"));
+
+        step("Set cookie to browser", () ->
+                getWebDriver().manage().addCookie(
+                        new Cookie("NOPCOMMERCE.AUTH", authorizationCookie)));
+        });
+
+
+        step("Open profile page", () ->
+                open("/customer/info"));
+
+        step("Clear email input", () ->
+                $("#Email").setValue(""));
+                $(".save-customer-info-button").click();
+
+        step("Verify error text", () ->
+                $(".field-validation-error span").shouldHave(text("Email is required.")));
+    }
+}
